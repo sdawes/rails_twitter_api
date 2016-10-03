9836af5073ff91cd9ef0baf5414c56a43fccff64
Stephen Dawes <sdawes@outlook.com>
2016-10-03 11:34:30 +0100

    remove follower model and controller

--- app/models/follower.rb
+++
@@ -1,4 +0,0 @@
-class Follower < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :twitter_uid, :name, :screen_name
-end
--- db/migrate/20161002212758_create_followers.rb
+++
@@ -1,9 +0,0 @@
-class CreateFollowers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :followers do |t|
-      t.string :followers
-
-      t.timestamps
-    end
-  end
-end
